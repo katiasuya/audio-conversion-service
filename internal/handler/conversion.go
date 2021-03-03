5f9c84d1cf9891f169074a2974305fa03a38580c
katiasuya <k.machalskaya@gmail.com>
2021-03-03 11:47:55 +0300

    change validation logic

--- internal/handler/conversion.go
+++ internal/handler/conversion.go
@@ -2,9 +2,7 @@
 package handler
 
 import (
-	"encoding/json"
 	"errors"
-	"io"
 	"net/http"
 	"strings"
 
@@ -23,60 +21,27 @@ func validateFormat(format string, targetFormat string) error {
 
 // Convert implements audio conversion.
 func Convert(w http.ResponseWriter, r *http.Request) {
-	reader, err := r.MultipartReader()
+	file, header, err := r.FormFile("file")
 	if err != nil {
 		web.RespondErr(w, http.StatusBadRequest, err)
 		return
 	}
+	defer file.Close()
 
-	type conversionRequest struct {
-		File         io.Reader `json:"file"`
-		TargetFormat string    `json:"targetFormat"`
-	}
-	var cr conversionRequest
-
-	var fileName string
-	for part, err := reader.NextPart(); err != io.EOF; part, err = reader.NextPart() {
-		if err != nil {
-			web.RespondErr(w, http.StatusBadRequest, err)
-			return
-		}
-
-		switch part.FormName() {
-		case "File":
-			cr.File = part
-			fileName = part.FileName()
-		case "TargetFormat":
-			err := json.NewDecoder(part).Decode(&cr.TargetFormat)
-			if err != nil {
-				web.RespondErr(w, http.StatusBadRequest, err)
-				return
-			}
-		default:
-			web.RespondErr(w, http.StatusBadRequest, errors.New("Unasked data"))
-			return
-		}
-	}
-
-	if fileName == "" {
-		web.RespondErr(w, http.StatusBadRequest, errors.New("file is missing"))
-		return
-	}
-
-	if cr.TargetFormat == "" {
+	targetFormat := r.FormValue("targetFormat")
+	if targetFormat == "" {
 		web.RespondErr(w, http.StatusBadRequest, errors.New("format is missing"))
 		return
 	}
 
-	fileParts := strings.Split(fileName, ".")
+	fileParts := strings.Split(header.Filename, ".")
 	name := fileParts[0]
 	if err := validateName(name); err != nil {
 		web.RespondErr(w, http.StatusBadRequest, err)
 		return
 	}
-
 	format := fileParts[1]
-	if err := validateFormat(format, cr.TargetFormat); err != nil {
+	if err := validateFormat(format, targetFormat); err != nil {
 		web.RespondErr(w, http.StatusBadRequest, err)
 		return
 	}
