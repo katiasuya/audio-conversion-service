bbbec566cd0f776e508645c9d260bc378dc5f44f
katiasuya <k.machalskaya@gmail.com>
2021-03-04 09:17:01 +0300

    add configuration

--- cmd/main.go
+++ cmd/main.go
@@ -3,9 +3,32 @@ package main
 import (
 	"log"
 
+	"github.com/joho/godotenv"
 	"github.com/katiasuya/audio-conversion-service/internal/app"
+	"github.com/kelseyhightower/envconfig"
 )
 
+// Config represents configuration parametres for db connection.
+type config struct {
+	Username string `required:"true"`
+	Password string `required:"true"`
+	Host     string `required:"true"`
+	Port     int    `required:"true"`
+}
+
 func main() {
+	var conf config
+	err := loadConfig(&conf)
+	if err != nil {
+		log.Fatal(err.Error())
+	}
+
 	log.Fatal(app.Run())
+}
+
+func loadConfig(c *config) error {
+	if err := godotenv.Load("../db.env"); err != nil {
+		return err
+	}
+	return envconfig.Process("Audio-converter", c)
 }
