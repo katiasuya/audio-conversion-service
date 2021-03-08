6e886a219a41e0b037f902b621824cdadce36bba
katiasuya <k.machalskaya@gmail.com>
2021-03-08 23:26:09 +0300

    add source and target formats to request table

--- scripts/queries.sql
+++ scripts/queries.sql
@@ -15,8 +15,8 @@
             INSERT INTO converter.audio (name, format, location) VALUES
             ('song name', 'MP3', 'location') RETURNING id)
 
-    INSERT INTO converter.request (user_id, original_id, converted_id, status)
-    SELECT '12feccec-3974-4dc2-ac63-b4838c7bf0eb', id, NULL,'queued'
+    INSERT INTO converter.request (user_id, source_id, source_format, target_id, target_format, status)
+    SELECT '12feccec-3974-4dc2-ac63-b4838c7bf0eb', id, 'MP3', NULL, 'WAV', 'queued'
     FROM audio_id;
 
    -- to list all the user's audios
@@ -24,7 +24,7 @@
     SELECT a.name
     FROM converter.audio a 
     JOIN converter.request r
-    ON a.id=r.original_id OR a.id=r.converted_id 
+    ON a.id=r.source_id OR a.id=r.target_id 
     WHERE r.user_id = '61e72557-e5af-4bc2-b19e-b1e4c7820d14';
 
    -- to get a particular user's audio
@@ -32,24 +32,24 @@
     SELECT *
     FROM converter.audio a 
     JOIN converter.request r
-    ON a.id=r.original_id OR a.id=r.converted_id 
+    ON a.id=r.source_id OR a.id=r.target_id 
     WHERE r.user_id = '61e72557-e5af-4bc2-b19e-b1e4c7820d14'
     AND a.id = '2a4159de-9f06-4920-a9f6-6f612fd0acf5';
 
    -- to get the request history 
    
-    SELECT a.name original, r.created, r.updated, r.status
+    SELECT a.name source, r.created, r.updated, r.status
     FROM converter.request r
     JOIN converter.audio a
-    ON a.id = r.original_id
+    ON a.id = r.source_id
     WHERE r.user_id='61e72557-e5af-4bc2-b19e-b1e4c7820d14';
 
    -- to get the request info of an audio
 
-    SELECT a.name original, r.created, r.updated, r.status
+    SELECT a.name source, r.created, r.updated, r.status
     FROM converter.request r
     JOIN converter.audio a
-    ON a.id = r.original_id
+    ON a.id = r.source_id
     WHERE r.user_id='61e72557-e5af-4bc2-b19e-b1e4c7820d14'
     AND a.id ='2a4159de-9f06-4920-a9f6-6f612fd0acf5';
 
@@ -85,7 +85,7 @@
                              JOIN converter."user" u
                              ON u.id = r.user_id 
                              JOIN converter.audio a
-                             ON a.id = r.original_id 
+                             ON a.id = r.source_id 
                              WHERE a.format='MP3');
 
 
