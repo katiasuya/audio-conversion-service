39342168ae3ba81114f9a8e55826fbd6d4402f8b
katiasuya <k.machalskaya@gmail.com>
2021-03-08 19:53:59 +0300

    refactor user validation

--- internal/server/validation.go
+++ internal/server/validation.go
@@ -2,38 +2,43 @@ package server
 
 import (
 	"errors"
-	"fmt"
 )
+
+const (
+	minLength         = 5
+	maxNameLength     = 256
+	maxPasswordLength = 128
+)
+
+var errInvalidLength = errors.New("invalid length")
 
 // ValidateUserCredentials validates user's credentials.
 func ValidateUserCredentials(username, password string) error {
 	if username == "" {
 		return errors.New("username is missing")
 	}
-	err := validate(username)
-	if err != nil {
+	if len(username) < minLength || len(username) > maxNameLength {
+		return errInvalidLength
+	}
+	if err := validateChars(username); err != nil {
 		return err
 	}
 
 	if password == "" {
 		return errors.New("password is missing")
 	}
-	err = validate(password)
-	if err != nil {
+	if len(password) < minLength || len(password) > maxPasswordLength {
+		return errInvalidLength
+	}
+	if err := validateChars(password); err != nil {
 		return err
 	}
 
 	return nil
 }
 
-func validate(str string) error {
-	const minLength, maxLength = 5, 15
-	if len(str) < minLength || len(str) > maxLength {
-		lenErr := fmt.Sprintf("%s: invalid length. It must be from 5 to 15", str)
-		return errors.New(lenErr)
-	}
-
+// validateChars checks whether the given string contains invalid characters.
+func validateChars(str string) error {
 	//return errors.New("invalid characters")
-
 	return nil
 }
