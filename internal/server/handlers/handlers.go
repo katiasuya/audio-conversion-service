1bedcc39da91eab1961f4c4768115db2a6ea5d20
katiasuya <k.machalskaya@gmail.com>
2021-04-18 12:34:54 +0300

    handlers to separate pkg

--- internal/server/handlers/handlers.go
+++ internal/server/handlers/handlers.go
@@ -12,6 +12,7 @@ import (
 	"github.com/katiasuya/audio-conversion-service/internal/auth"
 	"github.com/katiasuya/audio-conversion-service/internal/converter"
 	"github.com/katiasuya/audio-conversion-service/internal/repository"
+	"github.com/katiasuya/audio-conversion-service/internal/server"
 	ctx "github.com/katiasuya/audio-conversion-service/internal/server/context"
 	res "github.com/katiasuya/audio-conversion-service/internal/server/response"
 	"github.com/katiasuya/audio-conversion-service/internal/storage"
@@ -100,7 +101,7 @@ func (s *Server) SignUp(w http.ResponseWriter, r *http.Request) {
 	}
 	defer r.Body.Close()
 
-	if err := ValidateUserCredentials(req.Username, req.Password); err != nil {
+	if err := server.ValidateUserCredentials(req.Username, req.Password); err != nil {
 		s.logAndRespondErr(w, "invalid user credentials: ", err, http.StatusBadRequest)
 		return
 	}
@@ -192,7 +193,7 @@ func (s *Server) ConversionRequest(w http.ResponseWriter, r *http.Request) {
 	targetFormat := strings.ToLower(r.FormValue("targetFormat"))
 	filename := strings.TrimSuffix(header.Filename, "."+sourceFormat)
 
-	if err = ValidateRequest(filename, sourceFormat, targetFormat, sourceContentType[0]); err != nil {
+	if err = server.ValidateRequest(filename, sourceFormat, targetFormat, sourceContentType[0]); err != nil {
 		s.logAndRespondErr(w, "invalid request: ", err, http.StatusBadRequest)
 		return
 	}
@@ -284,75 +285,4 @@ func (s *Server) logAndRespondErr(w http.ResponseWriter, wrapper string, err err
 	errMsg := fmt.Errorf(wrapper+"%w", err)
 	s.logger.Errorln(errMsg)
 	res.RespondErr(w, code, errMsg)
-}
-
-const (
-	minLength = 6
-	maxLength = 128
-)
-const invalidChars = `:;<>\{}[]+=?&," `
-
-var formats = map[string]string{"mp3": "audio/mpeg", "wav": "audio/wave"}
-
-var (
-	ErrMissingUsername = errors.New("username is missing")
-	ErrMissingPassword = errors.New("password is missing")
-	ErrInvalidLength   = fmt.Errorf("invalid length: username and password must be from %d to %d characters", minLength, maxLength)
-	ErrInvalidChars    = fmt.Errorf("invalid character(s): you can't use %sor space character(s)", invalidChars)
-)
-
-// ValidateUserCredentials validates user's credentials.
-func ValidateUserCredentials(username, password string) error {
-	if username == "" {
-		return ErrMissingUsername
-	}
-	if len(username) < minLength || len(username) > maxLength {
-		return ErrInvalidLength
-	}
-
-	if containsInvalidChars(username) {
-		return ErrInvalidChars
-	}
-
-	if password == "" {
-		return ErrMissingPassword
-	}
-	if len(password) < minLength || len(password) > maxLength {
-		return ErrInvalidLength
-	}
-	if containsInvalidChars(password) {
-		return ErrInvalidChars
-	}
-
-	return nil
-}
-
-// ValidateRequest validates conversion request body.
-func ValidateRequest(name, sourceFormat, targetFormat, sourceContentType string) error {
-	if sourceFormat == "" {
-		return errors.New("source format is missing")
-	}
-	if targetFormat == "" {
-		return errors.New("target format is missing")
-	}
-	if formats[sourceFormat] != sourceContentType {
-		return errors.New("wrong source format for the file")
-	}
-	if sourceFormat == targetFormat {
-		return errors.New("source and target formats can't be equal")
-	}
-	if _, ok := formats[targetFormat]; !ok {
-		return errors.New("invalid target format, need mp3 or wav")
-	}
-
-	if containsInvalidChars(name) {
-		return ErrInvalidChars
-	}
-
-	return nil
-}
-
-// containsInvalidChars checks whether the given string contains invalid characters.
-func containsInvalidChars(str string) bool {
-	return strings.ContainsAny(str, invalidChars)
 }
