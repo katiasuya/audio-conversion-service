0637be9593c68f6b0697df8d2cbbab4863248047
katiasuya <k.machalskaya@gmail.com>
2021-04-29 12:22:29 +0300

    add logging middleware

--- internal/logging/logger.go
+++ internal/logging/logger.go
@@ -1,11 +1,18 @@
+// Package logging is used for handling structured logging in the application.
 package logging
 
 import (
+	"context"
 	"os"
 
 	log "github.com/sirupsen/logrus"
 )
 
+type key int
+
+const loggerKey key = 0
+
+// Init initializes the logger.
 func Init() *log.Logger {
 	logger := log.New()
 	logger.SetFormatter(&log.JSONFormatter{})
@@ -13,4 +20,13 @@ func Init() *log.Logger {
 	logger.SetLevel(log.DebugLevel)
 
 	return logger
+}
+
+func ContextWithLogger(ctx context.Context, logger *log.Logger) context.Context {
+	return context.WithValue(ctx, loggerKey, logger)
+}
+
+func LoggerFromContext(ctx context.Context) (*log.Logger, bool) {
+	logger, ok := ctx.Value(loggerKey).(*log.Logger)
+	return logger, ok
 }
