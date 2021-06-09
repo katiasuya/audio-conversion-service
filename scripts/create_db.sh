2f98c595cfabe9c4469b6ac88a0824b949c1567f
katiasuya <k.machalskaya@gmail.com>
2021-06-09 20:25:56 +0300

    double quote to prevent globbing in script

--- scripts/create_db.sh
+++ scripts/create_db.sh
@@ -7,8 +7,8 @@ read  DB_USERNAME
 echo 'Please, enter password:'
 read  -rs DB_PASSWORD
 
-psql postgres://postgres:$PASSWORD@localhost:5432 \
-	--variable=user_var=$DB_USERNAME \
-	--variable=password_var=$DB_PASSWORD \
+psql postgres://postgres:"$PASSWORD"@localhost:5432 \
+	--variable=user_var="$DB_USERNAME" \
+	--variable=password_var="$DB_PASSWORD" \
 	-f schema.sql
 
