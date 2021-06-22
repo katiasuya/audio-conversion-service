498726d4b62d2a78743e9e6a444db1e6061bff50
katiasuya <k.machalskaya@gmail.com>
2021-06-22 13:00:55 +0300

    delete validate

--- internal/server/validation_test.go
+++ internal/server/validation_test.go
@@ -50,7 +50,7 @@ func TestValidateUserCredentials(t *testing.T) {
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			res := ValidateUserCredentials(tt.username, tt.password)
+			res := UserCredentials(tt.username, tt.password)
 			if res != tt.exp {
 				t.Errorf("Expected %t, got %t", tt.exp, res)
 			}
