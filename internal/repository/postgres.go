8b7e21d2ead2f14397c08890514129b73871250a
katiasuya <k.machalskaya@gmail.com>
2021-06-29 22:41:55 +0300

    fix naming

--- internal/repository/postgres.go
+++ internal/repository/postgres.go
@@ -7,8 +7,8 @@ import (
 	"github.com/katiasuya/audio-conversion-service/internal/config"
 )
 
-// NewPostgresDB creates new database connection.
-func NewPostgresDB(c *config.Config) (*sql.DB, error) {
+// NewPostgresClient creates new postgres connection.
+func NewPostgresClient(c *config.Config) (*sql.DB, error) {
 	pqInfo := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=%s",
 		c.Host, c.Port, c.Username, c.Password, c.DBName, c.SSLMode)
 	db, err := sql.Open("postgres", pqInfo)
