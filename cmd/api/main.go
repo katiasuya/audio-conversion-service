ef37a53f4920278e2f6fe7d451decd76f8a33623
katiasuya <k.machalskaya@gmail.com>
2021-07-01 15:54:31 +0300

    add failing messages to main logs

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -2,11 +2,12 @@ package main
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/katiasuya/audio-conversion-service/internal/app"
 	"github.com/katiasuya/audio-conversion-service/internal/logger"
 )
 
 func main() {
-	logger.Fatal(context.Background(), app.RunAPI())
+	logger.Fatal(context.Background(), fmt.Errorf("API failed to start: %w", app.RunAPI()))
 }
--- cmd/converter/main.go
+++ cmd/converter/main.go
@@ -2,11 +2,12 @@ package main
 
 import (
 	"context"
+	"fmt"
 
 	"github.com/katiasuya/audio-conversion-service/internal/app"
 	"github.com/katiasuya/audio-conversion-service/internal/logger"
 )
 
 func main() {
-	logger.Fatal(context.Background(), app.RunConverter())
+	logger.Fatal(context.Background(), fmt.Errorf("converter failed to start: %w", app.RunConverter()))
 }
