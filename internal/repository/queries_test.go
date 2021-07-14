bb7e8032a68acd288b0e5d00204eeb041f566138
katiasuya <k.machalskaya@gmail.com>
2021-07-14 17:36:58 +0300

    remove test cases

--- internal/repository/queries_test.go
+++ internal/repository/queries_test.go
@@ -13,6 +13,29 @@ import (
 	"github.com/katiasuya/audio-conversion-service/internal/repository/model"
 )
 
+var (
+	audio = model.Audio{
+		ID:       "1",
+		Name:     "Yesterday",
+		Format:   "mp3",
+		Location: "location",
+	}
+	user = model.User{
+		ID:       "1",
+		Username: "user123",
+		Password: "qwerty123",
+	}
+	request = model.Request{
+		ID:           "1",
+		AudioName:    "Yesterday",
+		SourceFormat: "mp3",
+		TargetFormat: "wav",
+		Created:      time.Time{},
+		Updated:      time.Time{},
+		Status:       "queued",
+	}
+)
+
 func NewMock() (*sql.DB, sqlmock.Sqlmock) {
 	db, mock, err := sqlmock.New()
 	if err != nil {
@@ -23,92 +46,40 @@ func NewMock() (*sql.DB, sqlmock.Sqlmock) {
 	return db, mock
 }
 
-func TestGetAudioByID(t *testing.T) {
-	db, mock := NewMock()
-	repo := &Repository{db}
-	defer repo.Close()
-
-	cases := []struct {
-		name          string
-		expectedAudio model.Audio
-		prepare       func(model.Audio)
-	}{
-		{
-			name: "success",
-			expectedAudio: model.Audio{
-				ID:       "1",
-				Name:     "Yesterday",
-				Format:   "mp3",
-				Location: "location",
-			},
-			prepare: func(a model.Audio) {
-				mock.ExpectQuery("SELECT (.*) FROM converter.audio WHERE id = ?").
-					WithArgs(a.ID).
-					WillReturnRows(sqlmock.NewRows([]string{"id", "name", "format", "location"}).
-						AddRow(a.ID, a.Name, a.Format, a.Location))
-			},
-		},
-	}
-
-	for _, tc := range cases {
-		t.Run(tc.name, func(t *testing.T) {
-			tc.prepare(tc.expectedAudio)
-
-			gotAudio, err := repo.GetAudioByID(tc.expectedAudio.ID)
-			assertNoError(t, fmt.Errorf("error when getting audio by ID: '%w'", err))
-
-			if tc.expectedAudio != gotAudio {
-				t.Errorf("expected user to be %+v, but got %+v", tc.expectedAudio, gotAudio)
-			}
-
-			err = mock.ExpectationsWereMet()
-			assertNoError(t, fmt.Errorf("there were unfulfilled expectations: %w", err))
-		})
-	}
-}
-
 func TestGetIDAndPasswordByUsername(t *testing.T) {
 	db, mock := NewMock()
 	repo := &Repository{db}
 	defer repo.Close()
 
-	cases := []struct {
-		name         string
-		expectedUser model.User
-		prepare      func(model.User)
-	}{
-		{
-			name: "success",
-			expectedUser: model.User{
-				ID:       "1",
-				Username: "user123",
-				Password: "qwerty123",
-			},
-			prepare: func(u model.User) {
-				mock.ExpectQuery(`SELECT (.*) FROM converter."user" WHERE username=?`).
-					WithArgs(u.Username).
-					WillReturnRows(sqlmock.NewRows([]string{"id", "password"}).
-						AddRow(u.ID, u.Password))
-			},
-		},
-	}
+	t.Run("success", func(t *testing.T) {
+		mock.ExpectQuery(`SELECT (.*) FROM converter."user" WHERE username=?`).
+			WithArgs(user.Username).
+			WillReturnRows(sqlmock.NewRows([]string{"id", "password"}).
+				AddRow(user.ID, user.Password))
 
-	for _, tc := range cases {
-		t.Run(tc.name, func(t *testing.T) {
-			tc.prepare(tc.expectedUser)
+		gotID, gotPassword, err := repo.GetIDAndPasswordByUsername(user.Username)
+		assertNoError(t, fmt.Errorf("error when getting id and password: '%w'", err))
 
-			gotID, gotPassword, err := repo.GetIDAndPasswordByUsername(tc.expectedUser.Username)
-			assertNoError(t, fmt.Errorf("error when getting id and password: '%w'", err))
+		if user.ID != gotID || user.Password != gotPassword {
+			t.Errorf("expected user to have id=%s and password=%s, but got id=%s and password=%s",
+				user.ID, user.Password, gotID, gotPassword)
+		}
 
-			if tc.expectedUser.ID != gotID || tc.expectedUser.Password != gotPassword {
-				t.Errorf("expected user to have id=%s and password=%s, but got id=%s and password=%s",
-					tc.expectedUser.ID, tc.expectedUser.Password, gotID, gotPassword)
-			}
+		err = mock.ExpectationsWereMet()
+		assertNoError(t, fmt.Errorf("there were unfulfilled expectations: %w", err))
+	})
 
-			err = mock.ExpectationsWereMet()
-			assertNoError(t, fmt.Errorf("there were unfulfilled expectations: %w", err))
-		})
-	}
+	t.Run("no such user", func(t *testing.T) {
+		mock.ExpectQuery(`SELECT (.*) FROM converter."user" WHERE username=?`).
+			WithArgs(user.Username).
+			WillReturnRows(sqlmock.NewRows([]string{"id", "password"}))
+
+		_, _, err := repo.GetIDAndPasswordByUsername(user.Username)
+		assertError(t, fmt.Errorf("error when getting id and password: '%w'", err), ErrNoSuchUser)
+
+		err = mock.ExpectationsWereMet()
+		assertNoError(t, fmt.Errorf("there were unfulfilled expectations: %w", err))
+	})
 }
 
 func TestGetRequestHistory(t *testing.T) {
@@ -116,47 +87,58 @@ func TestGetRequestHistory(t *testing.T) {
 	repo := &Repository{db}
 	defer repo.Close()
 
-	cases := []struct {
-		name            string
-		expectedRequest model.Request
-		prepare         func(model.Request)
-	}{
-		{
-			name: "success",
-			expectedRequest: model.Request{
-				ID:           "1",
-				AudioName:    "Yesterday",
-				SourceFormat: "mp3",
-				TargetFormat: "wav",
-				Created:      time.Time{},
-				Updated:      time.Time{},
-				Status:       "queued",
-			},
-			prepare: func(r model.Request) {
-				mock.ExpectQuery(`SELECT r.id, a.name, r.source_format, r.target_format, r.created, r.updated, r.status
-				FROM converter.request r JOIN converter.audio a ON a.id = r.source_id WHERE r.user_id=?`).
-					WithArgs(r.UserID).
-					WillReturnRows(sqlmock.NewRows([]string{"id", "name", "source_format", "target_format", "created", "updated", "status"}).
-						AddRow(r.ID, r.AudioName, r.SourceFormat, r.TargetFormat, r.Created, r.Updated, r.Status))
-			},
-		},
-	}
+	t.Run("success", func(t *testing.T) {
+		mock.ExpectQuery(`SELECT r.id, a.name, r.source_format, r.target_format, r.created, r.updated, r.status
+		FROM converter.request r JOIN converter.audio a ON a.id = r.source_id WHERE r.user_id=?`).
+			WithArgs(request.UserID).
+			WillReturnRows(sqlmock.NewRows([]string{"id", "name", "source_format", "target_format", "created", "updated", "status"}).
+				AddRow(request.ID, request.AudioName, request.SourceFormat, request.TargetFormat, request.Created, request.Updated, request.Status))
 
-	for _, tc := range cases {
-		t.Run(tc.name, func(t *testing.T) {
-			tc.prepare(tc.expectedRequest)
+		gotRequest, err := repo.GetRequestHistory(request.UserID)
+		assertNoError(t, fmt.Errorf("error when getting audio by ID: '%w'", err))
 
-			gotRequest, err := repo.GetRequestHistory(tc.expectedRequest.UserID)
-			assertNoError(t, fmt.Errorf("error when getting audio by ID: '%w'", err))
+		if request != gotRequest[0] {
+			t.Errorf("expected user to be %+v, but got %+v", request, gotRequest)
+		}
 
-			if tc.expectedRequest != gotRequest[0] {
-				t.Errorf("expected user to be %+v, but got %+v", tc.expectedRequest, gotRequest)
-			}
+		err = mock.ExpectationsWereMet()
+		assertNoError(t, fmt.Errorf("there were unfulfilled expectations: %w", err))
+	})
 
-			err = mock.ExpectationsWereMet()
-			assertNoError(t, fmt.Errorf("there were unfulfilled expectations: %w", err))
-		})
-	}
+}
+func TestGetAudioByID(t *testing.T) {
+	db, mock := NewMock()
+	repo := &Repository{db}
+	defer repo.Close()
+
+	t.Run("success", func(t *testing.T) {
+		mock.ExpectQuery("SELECT (.*) FROM converter.audio WHERE id=?").
+			WithArgs(audio.ID).
+			WillReturnRows(sqlmock.NewRows([]string{"id", "name", "format", "location"}).
+				AddRow(audio.ID, audio.Name, audio.Format, audio.Location))
+
+		gotAudio, err := repo.GetAudioByID(audio.ID)
+		assertNoError(t, fmt.Errorf("error when getting audio by ID: '%w'", err))
+
+		if gotAudio != audio {
+			t.Errorf("expected user to be %+v, but got %+v", audio, gotAudio)
+		}
+
+		err = mock.ExpectationsWereMet()
+		assertNoError(t, fmt.Errorf("there were unfulfilled expectations: %w", err))
+	})
+
+	t.Run("no such audio", func(t *testing.T) {
+		mock.ExpectQuery("SELECT (.*) FROM converter.audio WHERE id=?").
+			WithArgs(audio.ID).
+			WillReturnRows(sqlmock.NewRows([]string{"id", "name", "format", "location"}))
+
+		_, err := repo.GetAudioByID(audio.ID)
+		assertError(t, fmt.Errorf("error when getting audio by ID: '%w'", err), ErrNoSuchAudio)
+
+		err = mock.ExpectationsWereMet()
+		assertNoError(t, fmt.Errorf("there were unfulfilled expectations: %w", err))
+	})
 }
 
 func TestInsertUser(t *testing.T) {
@@ -164,42 +146,22 @@ func TestInsertUser(t *testing.T) {
 	repo := &Repository{db}
 	defer repo.Close()
 
-	cases := []struct {
-		name         string
-		expectedUser model.User
-		prepare      func(model.User)
-	}{
-		{
-			name: "success",
-			expectedUser: model.User{
-				ID:       "1",
-				Username: "User123",
-				Password: "qwerty123",
-			},
-			prepare: func(u model.User) {
-				mock.ExpectQuery(`INSERT INTO converter."user" (.*) RETURNING`).
-					WithArgs(u.Username, u.Password).
-					WillReturnRows(sqlmock.NewRows([]string{"id"}).
-						AddRow(u.ID))
-			},
-		},
-	}
+	t.Run("success", func(t *testing.T) {
+		mock.ExpectQuery(`INSERT INTO converter."user" (.*) RETURNING`).
+			WithArgs(user.Username, user.Password).
+			WillReturnRows(sqlmock.NewRows([]string{"id"}).
+				AddRow(user.ID))
 
-	for _, tc := range cases {
-		t.Run(tc.name, func(t *testing.T) {
-			tc.prepare(tc.expectedUser)
+		userID, err := repo.InsertUser(user.Username, user.Password)
+		assertNoError(t, fmt.Errorf("error when inserting user: '%w'", err))
 
-			userID, err := repo.InsertUser(tc.expectedUser.Username, tc.expectedUser.Password)
-			assertNoError(t, fmt.Errorf("error when inserting user: '%w'", err))
+		if user.ID != userID {
+			t.Errorf("expected user id to be %q, but got %q", user.ID, userID)
+		}
 
-			if tc.expectedUser.ID != userID {
-				t.Errorf("expected user id to be %q, but got %q", tc.expectedUser.ID, userID)
-			}
-
-			err = mock.ExpectationsWereMet()
-			assertNoError(t, fmt.Errorf("there were unfulfilled expectations: %w", err))
-		})
-	}
+		err = mock.ExpectationsWereMet()
+		assertNoError(t, fmt.Errorf("there were unfulfilled expectations: %w", err))
+	})
 }
 
 func TestInsertAudio(t *testing.T) {
@@ -207,48 +169,34 @@ func TestInsertAudio(t *testing.T) {
 	repo := &Repository{db}
 	defer repo.Close()
 
-	cases := []struct {
-		name          string
-		expectedAudio model.Audio
-		prepare       func(model.Audio)
-	}{
-		{
-			name: "success",
-			expectedAudio: model.Audio{
-				ID:       "1",
-				Name:     "Yesterday",
-				Format:   "mp3",
-				Location: "location",
-			},
-			prepare: func(a model.Audio) {
-				mock.ExpectQuery(`INSERT INTO converter.audio (.+) RETURNING`).
-					WithArgs(a.Name, a.Format, a.Location).
-					WillReturnRows(sqlmock.NewRows([]string{"id"}).
-						AddRow(a.ID))
-			},
-		},
-	}
+	t.Run("success", func(t *testing.T) {
+		mock.ExpectQuery(`INSERT INTO converter.audio (.+) RETURNING`).
+			WithArgs(audio.Name, audio.Format, audio.Location).
+			WillReturnRows(sqlmock.NewRows([]string{"id"}).
+				AddRow(audio.ID))
 
-	for _, tc := range cases {
-		t.Run(tc.name, func(t *testing.T) {
-			tc.prepare(tc.expectedAudio)
+		audioID, err := repo.InsertAudio(audio.Name, audio.Format, audio.Location)
+		assertNoError(t, fmt.Errorf("error when inserting audio: '%w'", err))
 
-			audioID, err := repo.InsertAudio(tc.expectedAudio.Name, tc.expectedAudio.Format, tc.expectedAudio.Location)
-			assertNoError(t, fmt.Errorf("error when inserting audio: '%w'", err))
+		if audio.ID != audioID {
+			t.Errorf("expected audio id to be %q, but got %q", audio.ID, audioID)
+		}
 
-			if tc.expectedAudio.ID != audioID {
-				t.Errorf("expected audio id to be %q, but got %q", tc.expectedAudio.ID, audioID)
-			}
-
-			err = mock.ExpectationsWereMet()
-			assertNoError(t, fmt.Errorf("there were unfulfilled expectations: %w", err))
-		})
-	}
+		err = mock.ExpectationsWereMet()
+		assertNoError(t, fmt.Errorf("there were unfulfilled expectations: %w", err))
+	})
 }
 
 func assertNoError(t *testing.T, err error) {
 	t.Helper()
 	if errors.Unwrap(err) != nil {
 		t.Errorf("No error expected, got '%v' ", err)
+	}
+}
+
+func assertError(t *testing.T, err, assertErr error) {
+	t.Helper()
+	if errors.Unwrap(err) != assertErr {
+		t.Errorf("Expected '%v', got '%v' error", err, assertErr)
 	}
 }
